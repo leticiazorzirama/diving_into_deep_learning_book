b434b94405e9b2563d35776a5a6dee4fab666be3
Letícia Zorzi Rama <ramaleticia@univali.br>
2026-07-10 09:07:50 -0300

    Write and run 2.1.1 Getting Started codes

--- scripts/21_data_manipulation.py
+++ scripts/21_data_manipulation.py
@@ -1,4 +1,18 @@
+# 2.1. Data Manipulation
+# 2.1.1. Getting Started
+
 import torch
 import numpy
 
 x = torch.arange(12, dtype=torch.float32)
+x.numel()
+x.shape
+x
+
+X = x.reshape(3,4)
+X
+
+torch.zeros((2,3,4))
+torch.ones((2,3,4))
+torch.randn(3,4)
+torch.tensor([[2, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
