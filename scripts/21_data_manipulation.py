8e945f280bd997199d9c4a400c5cf68b65b06fc1
Letícia Zorzi Rama <ramaleticia@univali.br>
2026-07-16 10:08:38 -0300

    Write and run 2.1.4 Broadcasting, 2.1.5 Saving memory and 2.1.6 Conversion to Other Python Objects codes

--- scripts/21_data_manipulation.py
+++ scripts/21_data_manipulation.py
@@ -30,11 +30,12 @@ X[:2, :] # slice rows and select all columns
 X[:2, :] = 12 # assign new values for many indexes
 X
 
-# 2.1.3. Operations
+# 2.1.3. Operations 
 
 torch.exp(x) # elementwise unary scalar operators
 
 x, y = torch.tensor([1.0, 2, 4, 8]), torch.tensor([2, 2, 2, 2]) # elementwise binary scalar operators
+                                                                # implies vectors have the same shape
 x+y, x-y, x*y, x/y, x**y
 
 X = torch.arange(12, dtype=torch.float32).reshape((3,4))
@@ -47,3 +48,40 @@ torch.cat((X,Y), dim=1) # axis 1 = columns
 X == Y # binary tensors
 
 X.sum() # one element tensor
+
+# 2.1.4. Broadcasting (when shapes differ)
+
+a = torch.arange(3).reshape((3,1))
+b = torch.arange(2).reshape((1,2))
+
+a+b
+
+# 2.1.5. Saving Memory
+
+before = id(Y)
+before
+
+Y = Y+X
+
+id(Y) == before
+
+Z = torch.zeros_like(Y)
+print('id(Z)', id(Z))
+Z[:] = X+Y
+print('id(Z)', id(Z))
+
+before = id(X)
+X += Y
+id(X) == before
+
+# 2.1.6. Conversion to Other Python Objects
+A = X.numpy()
+B = torch.from_numpy(A)
+type(A), type(B)
+
+a = torch.tensor([3.5])
+a, a.item(), float(a), int(a)
+
+# 2.1.7. Summary
+
+# 2.1.8. Exercises
